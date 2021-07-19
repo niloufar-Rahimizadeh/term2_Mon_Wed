e9cb182a937616bfab291f88092825802efc7d0d
sinabakhshandeh <sina.bakhshandeh.t@gmail.com>
2021-07-19 17:33:19 +0430

    28Tir

---
+++ string_integer/test02.py
@@ -0,0 +1,14 @@
+name1 = "atbin and arad"
+name2 = "SEPITA"
+name3 = "yasna"
+name4 = "NiYaYeSh"
+
+out1 = name1.title()
+out2 = name2.lower()
+out3 = name3.upper()
+out4 = name4.swapcase()
+
+print(out1)
+print(out2)
+print(out3)
+print(out4)
---
+++ string_integer/test03.py
@@ -0,0 +1,9 @@
+sentence1 = "Arman is a good student"
+sentence2 = "Abtin can go to school now."
+sentence3 = "Elsa and Aysa are sisters."
+sentence4 = "I am learning English."
+
+
+name = sentence1[0:5]
+
+print(name)
