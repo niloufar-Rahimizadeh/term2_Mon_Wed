6ee5bc1361ba1f4087a53cc47e170f313e48b3b5
Niloufar_Rahimizadeh <niloufarrahimizade@yahoo.com>
2021-09-13 17:34:59 +0430

    22Shah

---
+++ turtle/test05.py
@@ -0,0 +1,46 @@
+import turtle
+
+app = turtle.Turtle()
+
+app.shape('turtle')
+app.shapesize(1)
+# app.speed(20)
+app.pencolor('#d459cd')
+app.pensize(5)
+
+app.circle(150)
+app.left(90)
+app.penup()
+app.forward(20)
+app.pendown()
+app.right(90)
+
+app.circle(130)
+app.left(90)
+app.penup()
+app.forward(20)
+app.pendown()
+app.right(90)
+
+app.circle(110)
+app.left(90)
+app.penup()
+app.forward(20)
+app.pendown()
+app.right(90)
+
+app.circle(90)
+app.left(90)
+app.penup()
+app.forward(20)
+app.pendown()
+app.right(90)
+
+app.circle(70)
+app.left(90)
+app.penup()
+app.forward(20)
+app.pendown()
+app.right(90)
+
+turtle.done()
